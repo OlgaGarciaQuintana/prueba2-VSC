017417d4a7f530ae9858003c47e813f9b6fc92f9
Olga <157001903+OlgaGarciaQuintana@users.noreply.github.com>
2025-02-05 21:46:29 +0100

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         //Esto es un hola mundo:
         System.out.println("Hello, World!");
+
+        Scanner entrada = new Scanner(System.in);
+		
+		//Variables:
+		double segundos, minutos, horas, días;
+		System.out.println("Introduce segundos:");
+		segundos = entrada.nextDouble();
+		
+		//Calcular minutos:
+		double total_minutos = segundos / 60;
+		System.out.println("Total minutos:" + total_minutos);
+		
+		//Calcular horas:
+		double total_horas = total_minutos / 60;
+		System.out.println("Total horas:" + total_horas);
+		
+		//Calcular días:
+		double total_días = total_horas / 24;
+		System.out.println("Total días:" + total_días);
     }
 }
